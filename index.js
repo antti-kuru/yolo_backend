39d5a27040fb10148bc038963e7ec5ecd7f99599
Antti Kuru <antti.i.kuru@gmail.com>
2024-01-10 17:06:30 +0200

    added reference to proposal model

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Proposal = require('./models/proposal')
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
+
+
 
 
 let proposals = [
